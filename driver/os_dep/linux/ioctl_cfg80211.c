0d4fc099bd7269789f6c03bde3928183995b486b
coolshou <coolshou2060@gmail.com>
2023-01-18 17:25:44 +0800

    fix kernel > 5.19.2
    cfg80211_ch_switch_notify() add link_id for MLO

--- driver/os_dep/linux/ioctl_cfg80211.c
+++ driver/os_dep/linux/ioctl_cfg80211.c
@@ -419,8 +419,11 @@ u8 rtw_cfg80211_ch_switch_notify(_adapter *adapter, u8 ch, u8 bw, u8 offset, u8 
 	if (ret != _SUCCESS)
 		goto exit;
 
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 2))
+	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0);
+	#else
 	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef);
-
+	#endif
 #else
 	int freq = rtw_ch2freq(ch);
 	enum nl80211_channel_type ctype;
