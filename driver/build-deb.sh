0b9093f3ef1a08a1a5e4a5aedea8d90146901613
coolshou <coolshou2060@gmail.com>
2023-01-19 09:58:06 +0800

    fix version format

--- driver/build-deb.sh
+++ driver/build-deb.sh
@@ -3,7 +3,7 @@
 dh_clean
 make clean
 #fakeroot debian/rules clean
-VERSION=`cat include/rtw_version.h | grep DRIVERVERSION | cut -d" " -f3 | cut -d"\"" -f2`
+VERSION=`cat include/rtw_version.h | grep DRIVERVERSION | cut -d" " -f2 | cut -d"\"" -f2 | cut -d"v" -f2`
 DRIVER_BUILD_VERSION=`cat include/version_build.h | grep 'STA_DRIVER_BUILD' | cut -d' ' -f3 |cut -d'"' -f2 | head -n1`
 sed -e "s|#VERSION#|${VERSION}|" \
 	-e "s|#DRIVER_BUILD_VERSION#|${DRIVER_BUILD_VERSION}|" \
